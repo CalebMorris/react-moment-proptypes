0eacfbe8c6130457b368f7c2b445c63a1bfcb4ba
Caleb Morris <caleb.morris.g@gmail.com>
2015-06-23 12:13:50 -0700

    Fixed to work with es5

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ module.exports = function validate(props, propName, componentName, location) {
   if (! moment.isMoment(propValue)) {
     var propType = typeof propValue;
     return new Error(
-      `Invalid ${location} \`${propName}\` of type \`${propType}\` ` +
-      `supplied to \`${componentName}\`, expected \`Moment\`.`
+      'Invalid ' + location + '`' + propName + '` of type `' + propType + '` ' +
+      'supplied to `' + componentName + '`, expected `Moment`.'
     );
   }
   return null;
-},
+};
+
