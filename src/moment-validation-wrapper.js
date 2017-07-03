01565020a953abca99af885f6a78d5ecc3fb1dd1
Caleb Morris <caleb.morris.g@gmail.com>
2017-07-02 18:39:38 -0700

    Added unit tests for moment validation wrapping
    Ignored dependency-specific lines that aren't against latest

--- src/moment-validation-wrapper.js
+++ src/moment-validation-wrapper.js
@@ -1,14 +1,20 @@
 var moment = require('moment');
 
 function isValidMoment(testMoment) {
+  if (typeof moment.isMoment === 'function' && !moment.isMoment(testMoment)) {
+    return false;
+  }
+
+  /* istanbul ignore else  */
   if (typeof testMoment.isValid === 'function') {
     // moment 1.7.0+
     return testMoment.isValid();
   }
 
-  return ! isNaN(testMoment);
+  /* istanbul ignore next */
+  return !isNaN(testMoment);
 }
 
 module.exports = {
   isValidMoment : isValidMoment,
-}
+};
